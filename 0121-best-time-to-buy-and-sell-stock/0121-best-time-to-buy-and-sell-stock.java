bf0602236179a01a8a52760a073236f5bfdd51a6
GOKUL A <gokulanbalagan1112@gmail.com>
2025-09-22 07:41:55 +0530

    Time: 2 ms (72.33%), Space: 61.4 MB (83.8%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int min = prices[0];
+        for(int price : prices){
+            max = Math.max(max, price - (min = (Math.min(min, price))));
+        }
+        return max;
+    }
+}
