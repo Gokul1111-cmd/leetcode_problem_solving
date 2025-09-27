84999f7aecee87573b6fb01cc74bf63edfafae28
GOKUL A <gokulanbalagan1112@gmail.com>
2025-09-27 14:14:53 +0530

    Time: 0 ms (100%), Space: 49.3 MB (37.02%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,15 +1,13 @@
 class Solution {
     public void reverseString(char[] s) {
-        int size = s.length;
-        int left = 0;
-        int right = size - 1;
-        while(left < right){
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            left++;
-            right--;
-        }
-        
+        int l = 0;
+        int r = s.length - 1;
+        while(l < r){
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }    
     }
 }
