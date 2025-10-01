3ccf2d75c4db51b2bda348619454f95ce2b91f88
GOKUL A <gokulanbalagan1112@gmail.com>
2025-10-01 11:27:55 +0530

    Time: 0 ms (100%), Space: 41.7 MB (61.78%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -3,7 +3,9 @@ class Solution {
         StringBuilder b = new StringBuilder();
         for(int i = 0 ; i < s.length() ; i++){
             char c = s.charAt(i);
-            c = Character.toLowerCase(c); 
+            if(c >= 65 && c <= 90){
+            c = (char)(c + 32);
+            }
             b.append(c);
         }
         return b.toString();
