b31f622f2d7cc47e85f99005fadad311553c8082
GOKUL A <gokulanbalagan1112@gmail.com>
2025-10-02 12:18:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int size = nums.length;
+        for(int i = 0 ; i < size - 1 ; i++){
+            for(int j = 0 ; j < size - i - 1; j++){
+                if(nums[j] > nums[j + 1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
