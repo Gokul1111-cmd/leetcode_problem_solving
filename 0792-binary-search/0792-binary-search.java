f25cd30d6cd1a04f50a4ec134096da013ed099a9
GOKUL A <gokulanbalagan1112@gmail.com>
2025-10-05 10:07:37 +0530

    Time: 0 ms (100%), Space: 46.1 MB (14.87%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,21 +1,22 @@
 class Solution {
-    public int binarySearch(int low, int high, int[] nums, int t){
+    public int binarySearch(int[] nums, int low , int high , int target){
         if(low > high){
             return -1;
         }
-        int mid = (low + high) / 2;
-        if(nums[mid] == t){
+        int mid = ( low + high ) / 2;
+        if(nums[mid] == target){
             return mid;
         }
-        if(nums[mid] < t){
-            return binarySearch(mid + 1, high, nums, t);
+        if(target < nums[mid]){
+            return binarySearch(nums, low, mid - 1, target);
         }
         else{
-            return binarySearch(low, mid - 1, nums, t);
+            return binarySearch(nums, mid + 1 , high, target);
         }
     }
     public int search(int[] nums, int target) {
-        int res = binarySearch(0,nums.length - 1 , nums, target);
+        int size = nums.length;
+        int res = binarySearch(nums, 0, size - 1, target);
         return res;
     }
 }
